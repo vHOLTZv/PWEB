4917ec27c6b3d4d20090d8900f7254e89d37e475
vHOLTZv <viniciusholtz10@gmail.com>
2026-06-01 10:45:03 -0300

    Update Exercicio4A.js

--- Pwebnode/Exercicios/Exercicio4A.js
+++ Pwebnode/Exercicios/Exercicio4A.js
@@ -2,3 +2,9 @@ function parte1(){
   for(let i =1; i<=10; i++){
     console.log("Primeira Parte:" + i);}}
 setTimeout(parte1,2000);
+fs.readFile('file.txt',(err,data) => {
+  if (err) throw err;
+  const registros = data.toString().split('\n');
+  registros.forEach((resgistro,index) => {
+    console.log("  segunda parte:" + index + " " + registro);});
+});
