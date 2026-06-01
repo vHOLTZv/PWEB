0350924313af3e143cdb3a9fe6f1ca0b0d87b350
vHOLTZv <viniciusholtz10@gmail.com>
2026-06-01 12:01:11 -0300

    Add basic HTTP server in Exercicio6.js

---
+++ Pwebnode/Exercicios/Exercicio6.js
@@ -0,0 +1,4 @@
+let http = require('http');
+let server = http.createServer(function(req,res){res.end("<html><body>Site da Fatec Sorocava</body></html>");});
+server.listen(3000);
+console.log('servidor iniciado!');
