5f7e587a2d6168c05a5c77a61b4c3e1279c34133
vHOLTZv <viniciusholtz10@gmail.com>
2026-06-01 10:27:05 -0300

    Create Callback1.js

---
+++ Pwebnode/Exercicios/Callback1.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+function saudacao(nome){
+  console.log('Oi ' + nome);
+}
+function entradaNome(callback){
+  let nome = prompt('Digite seu nome:');
+  callback(Nome);
+}
+entradaNome(saudacao);
